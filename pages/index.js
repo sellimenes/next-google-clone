f40f222a2a23a4cd6e51fecf0d80ab404ff2083b
sellimenes <sellim81@gmail.com>
2022-07-14 11:55:55 +0300

    added body

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Image from "next/image";
+import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -12,6 +14,26 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Body */}
+      <form className="flex flex-col items-center mt-40">
+        <Image
+          objectFit="cover"
+          width="450"
+          height="150"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+        />
+        <div className="flex w-full mx-auto mt-5 max-w-[90%] border border-gray-200 hover:shadow-lg focus:shadow-lg px-5 py-3 rounded-full cursor-pointer items-center sm:max-w-lg lg:max-w-2xl">
+          <SearchIcon className="h-5 text-gray-500 mr-3" />
+          <input
+            type="text"
+            className="flex-grow focus:outline-none cursor-pointer"
+          />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className="btn">Google Search</button>
+          <button className="btn">I Feel Lucky</button>
+        </div>
+      </form>
       {/* Footer */}
     </div>
   );
