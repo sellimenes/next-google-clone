e8fd99be469b46d6d5b7330b970a76698530b3d8
sellimenes <sellim81@gmail.com>
2022-07-15 16:13:07 +0300

    search logic added

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,16 @@ import Header from "../components/Header";
 import Image from "next/image";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 import Footer from "../components/Footer";
+import { useRouter } from "next/router";
+import { useRef } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+  const search = (event) => {
+    event.preventDeault();
+    const term = searchInputRef.current.value;
+  };
   return (
     <div>
       <Head>
@@ -25,13 +33,16 @@ export default function Home() {
         <div className="flex w-full mx-auto mt-5 max-w-[90%] border border-gray-200 hover:shadow-lg focus:shadow-lg px-5 py-3 rounded-full cursor-pointer items-center sm:max-w-lg lg:max-w-2xl">
           <SearchIcon className="h-5 text-gray-500 mr-3" />
           <input
+            ref={searchInputRef}
             type="text"
             className="flex-grow focus:outline-none cursor-pointer"
           />
           <MicrophoneIcon className="h-5" />
         </div>
         <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={search} className="btn">
+            Google Search
+          </button>
           <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
