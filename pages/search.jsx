bf79900da5763c65323ccaa4c3e5d25f96ee5be1
sellimenes <sellim81@gmail.com>
2022-07-17 16:05:26 +0300

    created the about data section

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,18 +1,22 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import SearchResults from "../components/SearchResults";
 import Response from "../Response";
+import {useRouter} from "next/router"
 
 export default function search({results}) {
   console.log(results);
+  const router = useRouter();
   return (
     <div>
       <Head>
-        <title>Search Page</title>
+        <title>{router.query.term} | Selim G-Clone</title>
       </Head>
 
       {/* Search Header */}
       <SearchHeader />
       {/* Search Result */}
+      <SearchResults results={results}/>
     </div>
   );
 }
